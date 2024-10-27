8753cfeff6f7537501723de935e65b0493f0ea0c
Farbod Rezabakhsh <farbod.rezabakhsh80@gmail.com>
2024-10-27 18:07:27 +0330

    Create Form views and also category views

--- A/urls.py
+++ A/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('Accounts.urls')),
-    path('',include('Form.urls'))
+    path('',include('Form.urls',namespace='Form'))
 ]
