482ab0390cd71144471c9b25154e4e34caeea70b
Farbod Rezabakhsh <farbod.rezabakhsh80@gmail.com>
2024-10-28 13:26:20 +0330

    Change category views into a viewsets function for better performance

--- Form/views.py
+++ Form/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from .serializers import FormSerializer,CategorySerializer
 from .models import Form,Category,Process
 from rest_framework import status
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -38,32 +40,34 @@ class FormDeleteView(APIView):
         form.delete()
         return Response({'message':'Form deleted!'})
 
-class CategoryListView(APIView):
-    def get(self,request):
-        category = Category.objects.all()
-        srz_data = CategorySerializer(instance=category,many=True)
+class CategoryViewSet(viewsets.ViewSet):
+    queryset = Category.objects.all()
+
+    def list(self,request):
+        srz_data = CategorySerializer(instance=self.queryset,many=True)
         return Response(data=srz_data.data)
 
-class CategoryCreateView(APIView):
-    def post(self,request):
+    def create(self, request):
         srz_data = CategorySerializer(data=request.data)
         if srz_data.is_valid():
             srz_data.save()
             return Response(data=srz_data.data,status=status.HTTP_201_CREATED)
         return Response(data=srz_data.errors,status=status.HTTP_400_BAD_REQUEST)
 
+    def retrieve(self,request,pk):
+        cat = get_object_or_404(self.queryset,pk=pk)
+        srz_data = CategorySerializer(instance=cat)
+        return Response(data=srz_data.data)
 
-class CategoryUpdateView(APIView):
-    def put(self,request,pk):
-        category = Category.objects.get(pk=pk)
-        ser_data = CategorySerializer(instance=category,data=category,partial=True)
-        if ser_data.is_valid():
-            ser_data.save()
-            return Response(ser_data.data)
-        return Response({'message':'you can not change form creator'})
+    def partial_update(self,request,pk):
+        cat = get_object_or_404(self.queryset,pk=pk)
+        srz_data = CategorySerializer(instance=cat,data=request.data,partial=True)
+        if srz_data.is_valid():
+            srz_data.save()
+            return Response(data=srz_data.data,status=status.HTTP_200_OK)
+        return Response(data=srz_data.errors,status=status.HTTP_400_BAD_REQUEST)
 
-class CategoryDeleteView(APIView):
-    def delete(self,request,pk):
-        category = Category.objects.get(pk=pk)
-        category.delete()
-        return Response({'message':'Category deleted!'})
+    def destroy(self,request,pk):
+        cat = get_object_or_404(self.queryset,pk=pk)
+        cat.delete()
+        return Response({'message':'category deleted'})
